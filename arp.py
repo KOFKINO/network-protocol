ea096dcf54fd43cb780794df756e330e9b911718
wqx.pc <kopfkino@yeah.net>
2021-12-09 00:12:00 +0800

    udp

--- arp.py
+++ arp.py
@@ -3,9 +3,7 @@ logging.getLogger("kamene.runtime").setLevel(logging.ERROR)  # 清除报错
 from kamene.all import *
 
 
-pkt = ARP()
-pkt.show()
-arp_pkt = ARP(op=2, psrc='192.168.56.100', hwdst='ff:ff:ff:ff:ff:ff', pdst='192.168.56.100')
+arp_pkt = ARP(op=2, psrc='192.168.186.100', hwdst='ff:ff:ff:ff:ff:ff', pdst='192.168.186.100')
 arp_pkt.show()
 for i in range(20):
-    sr1(pkt, timeout=2, verbose=False)
+    sr1(arp_pkt, timeout=2, verbose=False)
--- udp-client.py
+++ udp-client.py
@@ -14,7 +14,7 @@ def udp_send_data(ip, port, data_list):
         send_date = pickle.dumps(x)
         data_len = len(send_date)
         data_md5 = pickle.dumps(hashlib.md5(send_date).hexdigest())
-        packet = struct.pack('>2h2i2028s16p', version, pkt_type, seq_id, data_len, send_date, data_md5)
+        packet = struct.pack('>2h2i1024s128p', version, pkt_type, seq_id, data_len, send_date, data_md5)
         s.sendto(packet, address)
         seq_id += 1
     s.close()
@@ -22,4 +22,4 @@ def udp_send_data(ip, port, data_list):
 
 if __name__ == '__main__':
     user_data = ['自动化运维', [1, 'abc'], 3]
-    udp_send_data('120.27.210.240', 6666, user_data)
+    udp_send_data('192.168.186.1', 6666, user_data)
